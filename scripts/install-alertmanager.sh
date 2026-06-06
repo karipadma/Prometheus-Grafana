f931604232a2b502b2f059a0a9baa305d082798d
karipadma <41953175+karipadma@users.noreply.github.com>
2026-06-06 13:05:56 +0530

    Update install-alertmanager.sh

--- scripts/install-alertmanager.sh
+++ scripts/install-alertmanager.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-useradd --no-create-home alertmanager
-mkdir -p /etc/alertmanager /var/lib/alertmanager
+sudo useradd --no-create-home --shell /bin/false alertmanager || true
+
+sudo mkdir -p /etc/alertmanager
+sudo mkdir -p /var/lib/alertmanager
 
 cd /tmp
-wget https://github.com/prometheus/alertmanager/releases/latest/download/alertmanager-*.tar.gz
 
-tar -xzf alertmanager-*.tar.gz
-cp alertmanager-*/alertmanager /usr/local/bin/
+wget https://github.com/prometheus/alertmanager/releases/download/v0.28.1/alertmanager-0.28.1.linux-amd64.tar.gz
 
-systemctl daemon-reload
-systemctl enable alertmanager
-systemctl start alertmanager
+tar -xvzf alertmanager-0.28.1.linux-amd64.tar.gz
+
+sudo cp alertmanager-0.28.1.linux-amd64/alertmanager /usr/local/bin/
+sudo cp alertmanager-0.28.1.linux-amd64/amtool /usr/local/bin/
+
+sudo cp /home/ec2-user/Prometheus-Grafana/monitoring-server/alertmanager.yml /etc/alertmanager/
+sudo cp /home/ec2-user/Prometheus-Grafana/monitoring-server/alertmanager.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable alertmanager
+sudo systemctl start alertmanager
+
+sudo systemctl status alertmanager
