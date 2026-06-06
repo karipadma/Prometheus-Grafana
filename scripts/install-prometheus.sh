560535ff5e42d443812ec43f7ca7a31520f94cc8
karipadma <41953175+karipadma@users.noreply.github.com>
2026-06-06 12:46:49 +0530

    Update install-prometheus.sh

--- scripts/install-prometheus.sh
+++ scripts/install-prometheus.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-useradd --no-create-home prometheus
-mkdir -p /etc/prometheus /var/lib/prometheus
+sudo useradd --no-create-home --shell /bin/false prometheus || true
+
+sudo mkdir -p /etc/prometheus
+sudo mkdir -p /var/lib/prometheus
 
 cd /tmp
-wget https://github.com/prometheus/prometheus/releases/latest/download/prometheus-*.tar.gz
 
-tar -xzf prometheus-*.tar.gz
-cp prometheus-*/prometheus /usr/local/bin/
+wget https://github.com/prometheus/prometheus/releases/download/v3.4.2/prometheus-3.4.2.linux-amd64.tar.gz
 
-systemctl daemon-reload
-systemctl enable prometheus
-systemctl start prometheus
+tar -xvzf prometheus-3.4.2.linux-amd64.tar.gz
+
+sudo cp prometheus-3.4.2.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-3.4.2.linux-amd64/promtool /usr/local/bin/
+
+sudo cp /home/ec2-user/Prometheus-Grafana/monitoring-server/prometheus.yml /etc/prometheus/
+
+sudo cp /home/ec2-user/Prometheus-Grafana/monitoring-server/prometheus.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+
+sudo systemctl status prometheus
